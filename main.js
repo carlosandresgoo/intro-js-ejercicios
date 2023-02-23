14c6d6cb1e54cd1bb6f36b537e568daf910bb37c
Carlos Ruiz <carlosandresgoo@gmail.com>
2023-02-23 14:47:11 -0500

    ejercicios_js

---
+++ main.js
@@ -0,0 +1,155 @@
+// Punto 1
+// let miNombre = "Carlos";
+
+// Punto 2
+// let miApellido = "Ruiz";
+
+// Punto 3
+// let miEdad = "25";
+
+//punto 4
+// let miMascota = "Chepe"
+
+// Punto 5
+// let edadMascota = "4"
+
+// Punto 6
+// console.log(miNombre)
+// console.log(miApellido)
+// console.log(miEdad)
+// console.log(miMascota)
+// console.log(edadMascota)
+
+// Punto 7
+let miNombre = prompt("Ingresa tu nombre");
+let miApellido = prompt("Ingresa tu apellido");
+let miEdad = parseInt(prompt("Ingresa tu edad"));
+let miMascota = prompt("Ingresa el nombre de tu mascota");
+let edadMascota = parseInt(prompt("Ingresa la edad de tu mascota"));
+let nombreCompleto = miNombre + miApellido;
+
+// Punto 8
+let textoPresentacion = " Hola, mi nombre es " + nombreCompleto + " tengo " + miEdad + " años, mi mascota se llama " + miMascota + " y tiene " + edadMascota + " años.";
+
+console.log(textoPresentacion)
+
+// Punto 9
+// let sumaEdades = miEdad + edadMascota;
+// let restaEdades = miEdad - edadMascota;
+// let productoEdades = miEdad * edadMascota;
+// let divisionEdades = miEdad / edadMascota;
+// console.log(sumaEdades)
+// console.log(restaEdades)
+// console.log(productoEdades)
+// console.log(divisionEdades)
+
+
+// let  alumno = {
+//     nombre: miNombre,
+//     apellido: miApellido,
+//     edad: miEdad,
+//     edadMascota: edadMascota,
+
+// };
+// console.table(alumno)
+
+// // Punto 10
+// console.log(textoPresentacion);
+// console.log("La suma de edades es: " + sumaEdades);
+// console.log("La resta de edades es: " + restaEdades);
+// console.log("El producto de edades es: " + productoEdades);
+// console.log("La división de edades es: " + divisionEdades);
+
+// // Punto 11
+// console.table(alumno);
+// console.log("Nombre: " + alumno.nombre);
+// console.log("Apellido: " + alumno.apellido);
+// console.log("Edad: " + alumno.edad);
+// console.log("Edad de la mascota: " + alumno.edadMascota);
+// console.log("¿Es estudiante?: " + alumno.esEstudiante);
+// Punto 12
+// const mascota = {
+//     nombre: "Firulais",
+//     especie: "Perro",
+//     raza: "Labrador",
+//     edad: 3,
+//     vacunado: true,
+// };
+
+// console.table(mascota);
+// console.log(mascota.nombre);
+// console.log(mascota.especie);
+// console.log(mascota.raza);
+// console.log(mascota.edad);
+// console.log(mascota.vacunado);
+
+// Punto 13
+// const frutas = ["manzana", "banana", "naranja", "kiwi", "uva"];
+// console.log(frutas);
+// console.log(frutas[0]);
+// console.log(frutas[1]);
+// console.log(frutas[2]);
+// console.log(frutas[3]);
+// console.log(frutas[4]);
+
+// Punto 14
+// const numeros = [1, 3, 5, 7, 9];
+// console.log(numeros);
+// console.log(numeros[0]);
+// console.log(numeros[1]);
+// console.log(numeros[2]);
+// console.log(numeros[3]);
+// console.log(numeros[4]);
+
+// Punto 15
+// const familia = [
+//     { nombre: "Juan", edad: 50 },
+//     { nombre: "Ana", edad: 45 },
+//     { nombre: "Pedro", edad: 25 },
+//     { nombre: "Marta", edad: 22 },
+//     { nombre: "Jorge", edad: 18 },
+// ];
+// console.table(familia);
+// console.log(familia[0]);
+// console.log(familia[1]);
+// console.log(familia[2]);
+// console.log(familia[3]);
+// console.log(familia[4]);
+// Punto 16
+// const textoAleatorio = "Me gusta la " + frutas[1] +", tengo " + numeros[3] + " mascotas y mi hermano se llama " + familia[4].nombre;
+// console.log(textoAleatorio);
+
+//  Punto 17
+// const miEdad = parseInt(prompt("Ingrese su edad:"));
+// const edadCompanero = parseInt(prompt("Ingrese la edad de su compañero:"));
+
+// const edadesIguales = miEdad === edadCompanero;
+// const soyMayor = miEdad > edadCompanero;
+// const soyMenor = miEdad < edadCompanero;
+
+// console.log("Mi edad es igual a la de mi compañero:" + edadesIguales);
+// console.log("Mi edad es mayor a la de mi compañero:" + soyMayor);
+// console.log("Mi edad es menor a la de mi compañero:" + soyMenor);
+
+// Punto 18
+// const miEdad = parseInt(prompt("Ingrese su edad :"));
+// const soyMayorDeEdad = miEdad >= 18;
+// console.log("Soy mayor de edad:" + soyMayorDeEdad);
+
+// Punto 19
+// const edadPersona = parseInt(prompt("Ingrese la edad de la persona:"));
+// const alturaPersona = parseInt(prompt("Ingrese la altura de la persona en cm:"));
+
+// const puedeSubir = edadPersona >= 6 && alturaPersona >= 120;
+
+// console.log("La persona puede subir a la atracción :  " + puedeSubir);
+
+// Punto 20
+// const pasePersona = prompt("Ingrese el tipo de pase de la persona (VIP, NORMAL o LIMITADO):");
+// const saldoPersona = parseFloat(prompt("Ingrese el saldo de la persona:"));
+
+// const puedePasar = pasePersona === "VIP" || saldoPersona > 1000;
+
+// console.log("La persona puede pasar " + puedePasar);
+
+
